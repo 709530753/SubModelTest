523867d172dab848619be05d93e31eb461e9a7b8
myxc <70953073@qq.com>
2018-09-01 22:16:45 +0800

    commit

---
+++ SubModelTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubModelTest
+//
+//  Created by myxc on 2018/9/1.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
